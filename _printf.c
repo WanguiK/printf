b5bf4447271f92a27aeda0a8f2a9fdc4d5178886
WanguiK <wanguikings@gmail.com>
2023-05-29 04:52:03 -0700

    Task 0

--- _printf.c
+++ _printf.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
+#include <stdio.h>
 
 /**
  * _printf-  produces output according to a format
@@ -10,7 +11,6 @@
 int _printf(const char *format, ...)
 {
 	int count = 0;
-
 	va_list ap;
 
 	va_start(ap, format);
@@ -22,15 +22,16 @@ int _printf(const char *format, ...)
 			format++;
 			if (*format == 'c')
 			{
-				char val = va_arg(ap, int);
+				char c = va_arg(ap, int);
 
-				putchar(val);
+				putchar(c);
 				count++;
 			}
 			else if (*format == 's')
 			{
 				char *s = va_arg(ap, char *);
 
+				while (*s != '\0')
 				putchar(*s);
 				s++;
 				count++;
