b8b2adfe3b6b20a880c3c4435a48a5bbc9087ec3
WanguiK <wanguikings@gmail.com>
2023-05-30 02:41:47 -0700

    Task 0

--- _printf.c
+++ _printf.c
@@ -1,55 +1,55 @@
-#include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
  * _printf - produces output according to a format
  * @format: character string
- *
  * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
 	int count = 0;
-	char c;
-	const char *s;
 	va_list(ap);
+
+	if (format == NULL)
+	return (-1);
 
 	va_start(ap, format);
 
 	while (*format != '\0')
 	{
-		if (*format == '%')
+		if (*format != '%')
+		{
+			_putchar(*format);
+			count++;
+		}
+		else
 		{
 			format++;
-			switch (*format)
+			if (*format == '%')
 			{
-				case 'c':
-				c = va_arg(ap, int);
+				_putchar('%');
+				count++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(ap, int);
+
 				_putchar(c);
 				count++;
-				break;
+			}
+			else if (*format == 's')
+			{
+				const char *s = va_arg(ap, const char *);
 
-				case 's':
-				s = va_arg(ap, const char *);
 				while (*s != '\0')
 				{
 					_putchar(*s);
 					s++;
 					count++;
 				}
-				break;
-
-				case '%':
-				_putchar('%');
-				count++;
-				break;
 			}
-		}
-		else
-		{
-			_putchar(*format);
-			count++;
 		}
 		format++;
 	}
