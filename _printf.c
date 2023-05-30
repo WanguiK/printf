33f5e6b1b14adc097da2d20f433c77a25e8cb936
WanguiK <wanguikings@gmail.com>
2023-05-30 05:00:43 -0700

    Task 1

--- _printf.c
+++ _printf.c
@@ -10,10 +10,13 @@
 int _printf(const char *format, ...)
 {
 	int count = 0;
+
 	va_list(ap);
 
 	if (format == NULL)
-	return (-1);
+	{
+		return (-1);
+	}
 
 	va_start(ap, format);
 
@@ -48,6 +51,17 @@ int _printf(const char *format, ...)
 					_putchar(*s);
 					s++;
 					count++;
+				}
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(ap, int);
+
+				if (num < 0)
+				{
+					_putchar('-');
+					count++;
+					num = -num;
 				}
 			}
 		}
