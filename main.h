d2265b18e9bddca611fea573da6711dbbd4bb407
WanguiK <wanguikings@gmail.com>
2023-05-30 10:28:16 -0700

    Project

--- main.h
+++ main.h
@@ -7,6 +7,11 @@
 #include <limits.h>
 #include <unistd.h>
 
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
 
 /**
  * struct format - match the conversion specifiers for printf
